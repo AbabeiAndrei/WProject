5ebcf2017636c1dd0228944018cb3fbc1c8401ea
Ababei Andrei <ababeiandrei@outlook.com>
2016-07-10 11:45:17 +0300

    doc updates

---
+++ WProject/Controls/MainPageControls/TimeLineControls/ctrlTimeLineRowHeader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WProject.UiLibrary.Controls;
+
+namespace WProject.Controls.MainPageControls.TimeLineControls
+{
+    public partial class ctrlTimeLineRowHeader : WpUserControl
+    {
+        #region Constructrors
+
+        public ctrlTimeLineRowHeader()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Overrides of WpUserControl
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+
+        }
+
+        #endregion
+    }
+}
